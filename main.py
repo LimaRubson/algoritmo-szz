801401a20848617e5e40d2128489306cbafe500c
Douglas Almeida Vidal <vidalstm998@gmail.com>
2024-09-17 15:44:23 -0300

    1069 code error.

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ def get_bug_introducing_commits(repo_path, bug_reports):
     for bug_report in bug_reports:
         bug_id = bug_report
         bug_file = bug_report['']
-<<<<<<< HEAD
-        bug_line = bug_report['
-=======
-        bug_line = bug_report['']
->>>>>>> 4d54713222e9b09726f10d7b7680bc2df2c35f58
+        bug_line = bug_report
     
         # Obter os commits que modificaram o arquivo relevante para o bug
         blame_result = repo.blame('HEA', bug_file, L=bug_line)
@@ -24,7 +20,7 @@ def get_bug_introducing_commits(repo_path, bug_reports):
     return bug_introducing_commits
 
 # Exemplo de uso
-repo_path = '/caminho/para/repo'  # Substitua pelo caminho para o seu repositório
+repo_path  '/caminho/para/repo'  # Substitua pelo caminho para o seu repositório
 bug_reports = []
 
 bug_introducing_commits = get_bug_introducing_commits(repo_path, bug_reports)
