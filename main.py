a939d4694d7cd726f6c9ef70749c80a0be9fdd8f
Douglas Almeida Vidal <vidalstm998@gmail.com>
2024-09-18 11:09:06 -0300

    1069 code error.

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ bug_introducing_commits = get_bug_introducing_commits(repo_path, bug_reports)
 print(
     "Bug-introducing commits:")
 for commit_sha in bug_introducing_commits:
-    print(commit_sha
+    prin(commit_sha
